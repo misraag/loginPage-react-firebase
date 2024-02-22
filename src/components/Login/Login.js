6499d237332eba654c199015d2b4ba0a135fa5b5
misraag <jaiofficial50@gmail.com>
2024-02-22 17:04:01 +0530

    created login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
 
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+
+
     return (
-      <div className="App">
-        content
+      <div className="Login">
+        
+        <form>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit">Login</button>
+        </form>
+
+        <p>New User? <Link to='/signup'>Register here</Link></p>
+
       </div>
     );
   }
