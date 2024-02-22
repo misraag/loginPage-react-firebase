4fa4a7e11b3b599ef0560a5bf508986199f4dfd5
misraag <jaiofficial50@gmail.com>
2024-02-22 17:11:07 +0530

    created form for signup page

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from './Signup.module.css';
 
 
 function Signup() {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
     return (
-      <div className="App">
-        This is my second react app!
+      <div className="Signup">
+        
+        <form>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+          <button type="submit">Signup</button>
+        </form>
+
       </div>
     );
   }
