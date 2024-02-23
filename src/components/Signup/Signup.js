40e35ad1943a84486e4642e31d51b063227fd2e8
misraag <jaiofficial50@gmail.com>
2024-02-23 06:21:53 +0530

    finalised the styles for login and signup page

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from './Signup.module.css';
 import firebase from "firebase/compat/app";
+import { FaLock, FaUser } from "react-icons/fa";
 
 function Signup() {
 
@@ -24,14 +25,19 @@ function Signup() {
 
 
     return (
-      <div className="Signup">
+      <div className={styles.login}>
         
-        <form onSubmit={handleSignup}>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-          <button type="submit">Signup</button>
+        <form className={styles.signupform} onSubmit={handleSignup}>
+          <h1>Signup</h1>
+          <div className={styles.inputbox}>
+          <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+          <FaUser className={styles.icons}/>
+          </div>
+          <div className={styles.inputbox}>
+          <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <FaLock className={styles.icons}/>
+          </div>
+          <button type="submit">Register</button>
         </form>
 
       </div>
